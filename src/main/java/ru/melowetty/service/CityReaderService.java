39612f68998e47d6e6c7880b6d81117bee514d37
Denis Malinin <melowetty@mail.ru>
2024-09-02 14:38:29 +0500

    add city reader service

---
+++ src/main/java/ru/melowetty/service/CityReaderService.java
@@ -0,0 +1,38 @@
+package ru.melowetty.service;
+
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import ru.melowetty.model.City;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+@Slf4j
+@AllArgsConstructor
+public class CityReaderService {
+
+    protected ObjectMapper mapper;
+
+    public City readCityFromFile(String fileName) {
+        try {
+            log.debug("Начало считывания города из файла");
+
+            var city = mapper.readValue(Path.of(fileName).toFile(), City.class);
+
+            log.debug("Конец считывания города из файла");
+
+            return city;
+
+        } catch (DatabindException | StreamReadException exception) {
+            log.error("Произошла ошибка конвертации файла в объект");
+            log.error(exception.getMessage());
+        } catch (IOException ioException) {
+            log.error("Произошла ошибка во время чтения файла!");
+            log.error(ioException.getMessage());
+        }
+        return null;
+    }
+}
