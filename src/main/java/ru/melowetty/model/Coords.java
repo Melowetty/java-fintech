f016625f335638a9f2368ece33be83f564b5f126
Denis Malinin <melowetty@mail.ru>
2024-09-02 09:26:44 +0500

    add models

---
+++ src/main/java/ru/melowetty/model/Coords.java
@@ -0,0 +1,15 @@
+package ru.melowetty.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coords {
+    public float lat;
+    public float lon;
+}
