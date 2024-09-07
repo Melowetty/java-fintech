0336360bd6635ba5ad98a2d9f996f5e65d687ba4
Denis Malinin <melowetty@mail.ru>
2024-09-07 21:13:38 +0500

    fix imports

--- src/main/java/ru/melowetty/model/City.java
+++ src/main/java/ru/melowetty/model/City.java
@@ -1,6 +1,10 @@
 package ru.melowetty.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
