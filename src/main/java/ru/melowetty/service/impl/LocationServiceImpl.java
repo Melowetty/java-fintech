3a9abc13c450421ea7c6903ecd8012296689a47c
Denis Malinin <melowetty@mail.ru>
2024-09-25 23:12:40 +0500

    add location service data init

--- src/main/java/ru/melowetty/service/impl/LocationServiceImpl.java
+++ src/main/java/ru/melowetty/service/impl/LocationServiceImpl.java
@@ -1,20 +1,38 @@
 package ru.melowetty.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import ru.melowetty.controller.request.LocationPutRequest;
 import ru.melowetty.exception.EntityNotFoundException;
 import ru.melowetty.model.Location;
 import ru.melowetty.repository.LocationRepository;
+import ru.melowetty.service.KudagoService;
 import ru.melowetty.service.LocationService;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class LocationServiceImpl implements LocationService {
     private final LocationRepository locationRepository;
+    private final KudagoService kudagoService;
 
-    public LocationServiceImpl(LocationRepository locationRepository) {
+    public LocationServiceImpl(LocationRepository locationRepository, KudagoService kudagoService) {
         this.locationRepository = locationRepository;
+        this.kudagoService = kudagoService;
+        log.info("LocationServiceImpl created");
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void initialize() {
+        log.info("Инициализация городов запущена");
+        var locations = kudagoService.getLocations();
+        for (var location : locations) {
+            locationRepository.create(location);
+        }
+        log.info("Инициализация городов окончена, теперь городов: {}", locationRepository.findAll().size());
     }
 
     @Override
