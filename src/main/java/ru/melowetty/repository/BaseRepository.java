7bd339e9368558f9e6910c1ae8892f39c033ecb6
Denis Malinin <melowetty@mail.ru>
2024-09-25 21:41:23 +0500

    change signatures of methods in base repository

--- src/main/java/ru/melowetty/repository/BaseRepository.java
+++ src/main/java/ru/melowetty/repository/BaseRepository.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 public interface BaseRepository<E, I>{
     E create(E entity);
-    void removeById(E id);
+    void removeById(I id);
     E update(E entity);
     E findById(I id);
     List<E> findAll();
+    boolean existsById(I id);
 }
