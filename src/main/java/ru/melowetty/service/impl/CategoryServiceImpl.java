874d2ab5df9aa3585103c8e0560b6c8eb80ac689
Denis Malinin <melowetty@mail.ru>
2024-09-25 21:41:33 +0500

    add entity not found exception and category service impl

---
+++ src/main/java/ru/melowetty/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,61 @@
+package ru.melowetty.service.impl;
+
+import org.springframework.stereotype.Service;
+import ru.melowetty.controller.request.CategoryPutRequest;
+import ru.melowetty.exception.EntityNotFoundException;
+import ru.melowetty.model.Category;
+import ru.melowetty.repository.CategoryRepository;
+import ru.melowetty.service.CategoryService;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    private CategoryRepository categoryRepository;
+
+    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public List<Category> getCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category getCategoryById(int id) {
+        if(categoryRepository.existsById(id)) {
+            throw new EntityNotFoundException("Категория с таким идентификатором не найдена!");
+        }
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public Category createCategory(String slug, String name) {
+        var category = new Category();
+        category.setName(name);
+        category.setSlug(slug);
+        return categoryRepository.create(category);
+    }
+
+    @Override
+    public Category updateCategory(int id, CategoryPutRequest request) {
+        if(categoryRepository.existsById(id)) {
+            throw new EntityNotFoundException("Категория с таким идентификатором не найдена!");
+        }
+        var category = new Category();
+        category.setId(id);
+        category.setSlug(request.slug);
+        category.setName(request.name);
+        return categoryRepository.update(category);
+    }
+
+    @Override
+    public void deleteCategoryById(int id) {
+        if(categoryRepository.existsById(id)) {
+            throw new EntityNotFoundException("Категория с таким идентификатором не найдена!");
+        }
+
+        categoryRepository.removeById(id);
+    }
+}
