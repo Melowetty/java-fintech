adc7c3459c66b20b32f5d2b705e818f18c9bbc36
Denis Malinin <melowetty@mail.ru>
2024-10-02 21:24:02 +0500

    add category service test

--- src/main/java/ru/melowetty/model/Category.java
+++ src/main/java/ru/melowetty/model/Category.java
@@ -1,8 +1,14 @@
 package ru.melowetty.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Generated
 public class Category {
     public int id;
     public String slug;
