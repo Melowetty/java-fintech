757e0399b76dd9fae84e32d721165ea8aea1abe4
Denis Malinin <melowetty@mail.ru>
2024-10-09 20:49:24 +0500

    add currency utils test

--- currency-service/src/main/java/ru/melowetty/tinkofffintech/currencyservice/util/CurrencyUtils.java
+++ currency-service/src/main/java/ru/melowetty/tinkofffintech/currencyservice/util/CurrencyUtils.java
@@ -1,11 +1,12 @@
 package ru.melowetty.tinkofffintech.currencyservice.util;
 
 import java.util.Currency;
+import java.util.Locale;
 
 public class CurrencyUtils {
     public static Currency getCurrency(String targetCurrency) {
         try {
-            return Currency.getInstance(targetCurrency);
+            return Currency.getInstance(targetCurrency.toUpperCase());
         } catch (IllegalArgumentException e) {
             return null;
         }
