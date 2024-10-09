3280aed463a694c8e941e50ef13c77541d59b48a
Denis Malinin <melowetty@mail.ru>
2024-10-09 20:39:33 +0500

    remove unused tests and add test for currency converter service

--- currency-service/src/test/java/ru/melowetty/tinkofffintech/currencyservice/TestCurrencyServiceApplication.java
+++ currency-service/src/test/java/ru/melowetty/tinkofffintech/currencyservice/TestCurrencyServiceApplication.java
@@ -4,8 +4,4 @@ import org.springframework.boot.SpringApplication;
 
 public class TestCurrencyServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.from(CurrencyServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
-	}
-
 }
---
+++ currency-service/src/test/java/ru/melowetty/tinkofffintech/currencyservice/service/CurrencyConverterServiceTest.java
@@ -0,0 +1,37 @@
+package ru.melowetty.tinkofffintech.currencyservice.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+
+@ExtendWith(MockitoExtension.class)
+public class CurrencyConverterServiceTest {
+    @InjectMocks
+    CurrencyConverterService currencyConverterService;
+
+    @Mock
+    CurrencyService currencyService;
+
+    @Test
+    public void test_currency_converting() {
+        var fromCurrency = Currency.getInstance("USD");
+        var toCurrency = Currency.getInstance("EUR");
+        var amount = BigDecimal.valueOf(10);
+
+        Mockito.when(currencyService.getCurrencyRate(fromCurrency)).thenReturn(BigDecimal.valueOf(80));
+        Mockito.when(currencyService.getCurrencyRate(toCurrency)).thenReturn(BigDecimal.valueOf(100));
+
+        BigDecimal actual = currencyConverterService.convertCurrency(fromCurrency, toCurrency, amount);
+        BigDecimal expected = BigDecimal.valueOf(8);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
