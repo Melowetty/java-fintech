13972b60bb958e017b2432b40fcff5e3c8814f13
Denis Malinin <melowetty@mail.ru>
2024-11-01 01:52:58 +0500

    add get events from kudago api

--- main-service/src/main/java/ru/melowetty/service/impl/KudagoServiceImpl.java
+++ main-service/src/main/java/ru/melowetty/service/impl/KudagoServiceImpl.java
@@ -9,12 +9,20 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import ru.melowetty.model.Category;
+import ru.melowetty.model.Event;
 import ru.melowetty.model.Location;
 import ru.melowetty.service.KudagoService;
 
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Semaphore;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @Slf4j
@@ -90,6 +98,61 @@ public class KudagoServiceImpl implements KudagoService {
         return List.of();
     }
 
+    public List<Event> getEvents(LocalDate dateFrom, LocalDate dateTo, int page) {
+        try {
+            semaphore.acquire();
+
+            var variables = new HashMap<String, String>();
+            variables.put("actual_since", dateFrom.toString());
+            variables.put("actual_until", dateTo.toString());
+            variables.put("page", String.valueOf(page));
+            variables.put("page_size", "100");
+            variables.put("text_format", "text");
+            variables.put("fields", "id,title,price,is_free,dates");
+
+            var response = retryTemplate.execute(context ->
+                    restTemplate.getForObject(BASE_URL + "/events", PagedKudagoEventsResponse.class, variables));
+
+            if (response == null) {
+                throw new RestClientException("Ответ от Kudago API пустой");
+            }
+
+            return response.results.stream().map(event -> {
+                var dates = event.dates.stream().map((date) -> new Event.EventDates(date.start, date.end))
+                        .toList();
+                return Event.builder()
+                        .id(event.id)
+                        .title(event.title)
+                        .price(getPriceOfEvent(event))
+                        .dates(dates)
+                        .build();
+            }).toList();
+
+
+        } catch (RestClientException | InterruptedException e) {
+            log.error("Произошла ошибка во время получения данных об событиях из Kudago API", e);
+        } finally {
+            semaphore.release();
+        }
+
+        return List.of();
+    }
+
+    private BigDecimal getPriceOfEvent(KudagoEvent event) {
+        if (event == null || event.price.isEmpty()) return null;
+        if (event.isFree) return BigDecimal.ZERO;
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(event.price);
+
+        BigDecimal lastNumber = null;
+
+        while (matcher.find()) {
+            lastNumber = new BigDecimal(matcher.group());
+        }
+
+        return lastNumber;
+    }
+
     @Data
     static class KudagoLocationResponse {
         public String slug;
@@ -101,5 +164,32 @@ public class KudagoServiceImpl implements KudagoService {
         public int id;
         public String slug;
         public String name;
+    }
+
+    @Data
+    static class KudagoEvent {
+        public int id;
+
+        public String title;
+
+        public String price;
+
+        public boolean isFree;
+
+        public List<KudagoDate> dates;
+    }
+
+    @Data
+    static class KudagoDate {
+        public Instant start;
+        public Instant end;
+    }
+
+    @Data
+    static class PagedKudagoEventsResponse {
+        private final List<KudagoEvent> results = new ArrayList<>();
+        private int count;
+        private String next;
+        private String previous;
     }
 }
