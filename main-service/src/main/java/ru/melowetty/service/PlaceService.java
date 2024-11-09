f7b6e0661d7382fb11b579aef0d681d9a8114eda
Denis Malinin <melowetty@mail.ru>
2024-11-09 05:36:03 +0500

    add impl of entity services

--- main-service/src/main/java/ru/melowetty/service/PlaceService.java
+++ main-service/src/main/java/ru/melowetty/service/PlaceService.java
@@ -1,7 +1,11 @@
 package ru.melowetty.service;
 
 import org.springframework.stereotype.Service;
+import ru.melowetty.entity.Place;
+import ru.melowetty.exception.EntityNotFoundException;
 import ru.melowetty.repository.PlaceRepository;
+
+import java.util.List;
 
 @Service
 public class PlaceService {
@@ -11,5 +15,37 @@ public class PlaceService {
         this.placeRepository = placeRepository;
     }
 
+    public Place createPlace(String name, String slug) {
+        var place = new Place();
 
+        place.setName(name);
+        place.setSlug(slug);
+        return placeRepository.save(place);
+    }
+
+    public Place getPlaceById(Long id) {
+        return placeRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Место с таким ID не найдено!"));
+    }
+
+    public void deletePlaceById(Long id) {
+        if (!placeRepository.existsById(id)) {
+            throw new EntityNotFoundException("Место с таким ID не найдено!");
+        }
+
+        placeRepository.deleteById(id);
+    }
+
+    public List<Place> getAllPlaces() {
+        return placeRepository.findAll();
+    }
+
+    public Place updatePlace(Long id, String name, String slug) {
+        var place = getPlaceById(id);
+
+        place.setName(name);
+        place.setSlug(slug);
+
+        return placeRepository.save(place);
+    }
 }
