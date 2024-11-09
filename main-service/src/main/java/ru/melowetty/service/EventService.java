b1172743cb60eaee1fa5f99e5e46469fa66a1433
Denis Malinin <melowetty@mail.ru>
2024-11-09 13:42:15 +0500

    add filtering all events by fields

--- main-service/src/main/java/ru/melowetty/service/EventService.java
+++ main-service/src/main/java/ru/melowetty/service/EventService.java
@@ -1,14 +1,18 @@
 package ru.melowetty.service;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import ru.melowetty.entity.Event;
 import ru.melowetty.entity.Place;
+import ru.melowetty.entity.Event_;
+import ru.melowetty.entity.Place_;
 import ru.melowetty.exception.EntityNotFoundException;
 import ru.melowetty.exception.RelatedEntityNotFoundException;
 import ru.melowetty.repository.EventRepository;
 import ru.melowetty.repository.PlaceRepository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -46,6 +50,36 @@ public class EventService {
 
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
+    }
+
+    public List<Event> filterAllEvents(String name, Long placeId, LocalDateTime fromDate, LocalDateTime toDate) {
+        var specifications = new ArrayList<Specification<Event>>();
+
+        if (name != null) {
+            Specification<Event> filterByName = ((root, query, criteriaBuilder) ->
+                    criteriaBuilder.like(root.get(Event_.NAME), "%" + name + "%"));
+            specifications.add(filterByName);
+        }
+
+        if (placeId != null) {
+            Specification<Event> filterByPlace = (((root, query, criteriaBuilder) ->
+                    criteriaBuilder.equal(root.get(Event_.PLACE).get(Place_.ID), placeId)));
+            specifications.add(filterByPlace);
+        }
+
+        if (fromDate != null) {
+            Specification<Event> filterByFromDate = ((root, query, criteriaBuilder) ->
+                    criteriaBuilder.greaterThanOrEqualTo(root.get(Event_.DATE), fromDate));
+            specifications.add(filterByFromDate);
+        }
+
+        if (toDate != null) {
+            Specification<Event> filterByToDate = ((root, query, criteriaBuilder) ->
+                    criteriaBuilder.lessThanOrEqualTo(root.get(Event_.DATE), toDate));
+            specifications.add(filterByToDate);
+        }
+
+        return eventRepository.findAll(Specification.allOf(specifications));
     }
 
     public Event updateEvent(Long id, String name, LocalDateTime date, Long placeId) {
