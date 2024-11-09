38723bfa576ac82040571410bc8d919674e9b47e
Denis Malinin <melowetty@mail.ru>
2024-11-09 05:36:03 +0500

    add impl of entity services

--- main-service/src/main/java/ru/melowetty/service/EventService.java
+++ main-service/src/main/java/ru/melowetty/service/EventService.java
@@ -1,13 +1,66 @@
 package ru.melowetty.service;
 
 import org.springframework.stereotype.Service;
+import ru.melowetty.entity.Event;
+import ru.melowetty.entity.Place;
+import ru.melowetty.exception.EntityNotFoundException;
+import ru.melowetty.exception.RelatedEntityNotFoundException;
 import ru.melowetty.repository.EventRepository;
+import ru.melowetty.repository.PlaceRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EventService {
     private final EventRepository eventRepository;
+    private final PlaceRepository placeRepository;
 
-    public EventService(EventRepository eventRepository) {
+    public EventService(EventRepository eventRepository, PlaceRepository placeRepository) {
         this.eventRepository = eventRepository;
+        this.placeRepository = placeRepository;
+    }
+
+    public Event createEvent(String name, LocalDateTime date, Long placeId) {
+        var event = new Event();
+        var place = getPlaceById(placeId);
+
+        event.setName(name);
+        event.setPlace(place);
+        event.setDate(date);
+        return eventRepository.save(event);
+    }
+
+    public Event getEventById(Long id) {
+        return eventRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Событие с таким ID не найдено!"));
+    }
+
+    public void deleteEventById(Long id) {
+        if (!eventRepository.existsById(id)) {
+            throw new EntityNotFoundException("Событие с таким ID не найдено!");
+        }
+
+        eventRepository.deleteById(id);
+    }
+
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    public Event updateEvent(Long id, String name, LocalDateTime date, Long placeId) {
+        var event = getEventById(id);
+        var place = getPlaceById(placeId);
+
+        event.setName(name);
+        event.setDate(date);
+        event.setPlace(place);
+
+        return eventRepository.save(event);
+    }
+
+    public Place getPlaceById(Long id) {
+        return placeRepository.getPlaceById(id)
+                .orElseThrow(() -> new RelatedEntityNotFoundException("Место с таким ID не найдено!"));
     }
 }
