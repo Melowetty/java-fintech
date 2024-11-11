8348e338134f4093085232ff79e68696865929b7
Denis Malinin <melowetty@mail.ru>
2024-11-11 13:13:19 +0500

    add event controller

--- main-service/src/main/java/ru/melowetty/controller/EventController.java
+++ main-service/src/main/java/ru/melowetty/controller/EventController.java
@@ -1,86 +1,68 @@
 package ru.melowetty.controller;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
-import ru.melowetty.model.EventDto;
-import ru.melowetty.service.impl.NearestEventsService;
+import ru.melowetty.controller.request.EventCreateRequest;
+import ru.melowetty.controller.request.EventPutRequest;
+import ru.melowetty.entity.Event;
+import ru.melowetty.service.EventService;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Currency;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 @RestController
-@RequestMapping("events")
+@RequestMapping("event")
 @Valid
 public class EventController {
-    private final NearestEventsService eventsService;
+    private final EventService eventService;
 
-    public EventController(NearestEventsService eventsService) {
-        this.eventsService = eventsService;
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
     }
 
-    @GetMapping("/future")
-    public CompletableFuture<List<EventDto>> completableFutureGetEventsByBudget(
-            @NotNull(message = "Не указан бюджет")
-                    @RequestParam("budget")
-                    @DecimalMin(value = "0.0", message = "Бюджет не должен быть меньше нуля!")
-            BigDecimal budget,
-
-            @RequestParam("currency")
-            String currencyRaw,
-
-            @RequestParam(value = "dateFrom", required = false)
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate dateFrom,
-
-            @RequestParam(value = "dateTo", required = false)
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate dateTo
+    @GetMapping
+    public List<Event> getAllEvents(
+            @RequestParam(required = false)
+            String name,
+            @RequestParam(required = false)
+            Long placeId,
+            @RequestParam(required = false)
+            LocalDate fromDate,
+            @RequestParam(required = false)
+            LocalDate toDate
     ) {
-        Currency currency = Currency.getInstance(currencyRaw);
-
-        if (dateFrom == null || dateTo == null) {
-            dateFrom = LocalDate.now();
-            dateTo = LocalDate.now().plusDays(7);
-        }
-
-        return eventsService.getEventsByBudgetFuture(budget, currency, dateFrom, dateTo);
+        return eventService.filterAllEvents(name, placeId, fromDate.atStartOfDay(), toDate.atTime(23, 59, 59));
     }
 
-    @GetMapping("/reactor")
-    public Flux<EventDto> reactorGetEventsByBudget(
-            @NotNull(message = "Не указан бюджет")
-            @RequestParam("budget")
-            @DecimalMin(value = "0.0", message = "Бюджет не должен быть меньше нуля!")
-            BigDecimal budget,
+    @GetMapping("/{id}")
+    public Event getEvent(@PathVariable Long id) {
+       return eventService.getEventById(id);
+    }
 
-            @RequestParam("currency")
-            String currencyRaw,
+    @PostMapping
+    public Event createEvent(@RequestBody EventCreateRequest request) {
+        return eventService.createEvent(
+                request.name,
+                request.date,
+                request.placeId
+        );
+    }
 
-            @RequestParam(value = "dateFrom", required = false)
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate dateFrom,
+    @DeleteMapping("/{id}")
+    public void deleteEvent(@PathVariable Long id) {
+        eventService.deleteEventById(id);
+    }
 
-            @RequestParam(value = "dateTo", required = false)
-            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-            LocalDate dateTo
-    ) {
-        Currency currency = Currency.getInstance(currencyRaw);
-
-        if (dateFrom == null || dateTo == null) {
-            dateFrom = LocalDate.now();
-            dateTo = LocalDate.now().plusDays(7);
-        }
-
-        return eventsService.getEventsByBudgetReactor(budget, currency, dateFrom, dateTo);
+    @PutMapping("/{id}")
+    public Event updateEvent(@PathVariable Long id, @RequestBody EventPutRequest request) {
+        return eventService.updateEvent(id, request.name, request.date, request.placeId);
     }
 }
--- main-service/src/main/java/ru/melowetty/controller/request/EventPutRequest.java
+++ main-service/src/main/java/ru/melowetty/controller/request/EventPutRequest.java
@@ -1,9 +1,13 @@
 package ru.melowetty.controller.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Generated;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import java.time.LocalDateTime;
 
@@ -12,7 +16,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Generated
 public class EventPutRequest {
+    @NotNull(message = "Не указано название мероприятия")
+    @Length(min = 3, message = "Длина названия мероприятия не может быть меньше 3-х символов")
+    @NotBlank(message = "Название мероприятия не может быть пустым")
     public String name;
+
+    @NotNull(message = "Не указана дата проведения мероприятия")
     public LocalDateTime date;
+
+    @NotNull(message = "Не указано место проведения мероприятия")
+    @Min(value = 1, message = "ID места проведения должен быть больше 0")
     public Long placeId;
 }
