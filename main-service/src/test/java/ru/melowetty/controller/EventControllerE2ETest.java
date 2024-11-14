d7fca416b0beb62fa81dd52087d370f923908258
Denis Malinin <melowetty@mail.ru>
2024-11-14 18:15:27 +0500

    fix tests

--- main-service/src/test/java/ru/melowetty/controller/EventControllerE2ETest.java
+++ main-service/src/test/java/ru/melowetty/controller/EventControllerE2ETest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 )
 @Testcontainers
 @SpringBootTest
+@ActiveProfiles("test-with-db")
 @AutoConfigureMockMvc
 public class EventControllerE2ETest {
     @Autowired
