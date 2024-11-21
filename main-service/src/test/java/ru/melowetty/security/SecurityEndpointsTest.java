f639d41fee5ffdf3012f99ef324a057906e7b498
Denis Malinin <melowetty@mail.ru>
2024-11-22 03:09:25 +0500

    add tests for securing endpoints

---
+++ main-service/src/test/java/ru/melowetty/security/SecurityEndpointsTest.java
@@ -0,0 +1,46 @@
+package ru.melowetty.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class SecurityEndpointsTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void attemptAccessToSecuredEndpoint() throws Exception {
+        mockMvc.perform(get("/api/"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser()
+    @Test
+    public void successAttemptAccessToSecuredEndpoint() throws Exception {
+        mockMvc.perform(get("/api/"))
+                .andExpect(status().isNotFound());
+    }
+
+    @WithMockUser()
+    @Test
+    public void unSuccessAttemptAccessToSecuredEndpointForAdmins() throws Exception {
+        mockMvc.perform(post("/api/"))
+                .andExpect(status().isForbidden());
+    }
+
+    @WithMockUser(roles = "ADMIN")
+    @Test
+    public void successAttemptAccessToSecuredEndpointForAdmins() throws Exception {
+        mockMvc.perform(post("/api/"))
+                .andExpect(status().isNotFound());
+    }
+}
